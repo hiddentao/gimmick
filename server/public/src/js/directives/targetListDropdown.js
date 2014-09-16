e9d2b0a63075ffff111f8ecec8f216f690d2d139
Ramesh Nair <ram@hiddentao.com>
2014-09-16 13:42:18 +0800

    Know when target has changed.

--- server/public/src/js/directives/targetListDropdown.js
+++ server/public/src/js/directives/targetListDropdown.js
@@ -2,25 +2,37 @@ angular.module('gimmick')
   
   .directive('targetListDropdown', function() {
     return {
-        restrict: 'E',
-        templateUrl: 'directives/target-list-dropdown.html',
-        link: function(scope, element) {
-          $('.dropdown', element).dropdown();
-        },
-        controller: function($scope, $element, $rootScope, Target) {
-          $rootScope.$on('targets updated', function(e, t) {
-            $scope.targets = Target.getAll();
-            $scope.$apply();
-
-            setTimeout(function() {
-              $('.dropdown', $element).dropdown();
-
-              var ids = Object.keys($scope.targets);
-              if (1 === ids.length) {
-                $('.dropdown', $element).dropdown('set selected', ids[0]);
-              }
-            }, 100);
-          });
+      restrict: 'E',
+      templateUrl: 'directives/target-list-dropdown.html',
+      controller: function($scope, $element, $rootScope, $location, Target) {
+        var onChanged = function(id) {
+          location.path('/log/' + id);
         }
+
+        var initDropdown = function() {
+          $('.dropdown', $element).dropdown({
+            onChange: onChanged
+          });
+        };
+
+        // init dropdown
+        initDropdown();
+
+        // when new target added
+        $rootScope.$on('targets updated', function(e, t) {
+          $scope.targets = Target.getAll();
+          $scope.$apply();
+
+          initDropdown();
+
+          var ids = Object.keys($scope.targets);
+          if (1 === ids.length) {
+            $('.dropdown', $element).dropdown('set selected', ids[0]);
+
+            onChanged(ids[0]);
+          }
+        });
+      }
     };
   });
+
